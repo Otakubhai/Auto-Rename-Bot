845ec288af93b8a1b473318e958d72a16abc626b
Otakubhai <87870903+Otakubhai@users.noreply.github.com>
2024-02-25 15:41:22 +0530

    Update start_&_cb.py

--- plugins/start_&_cb.py
+++ plugins/start_&_cb.py
@@ -10,13 +10,13 @@ async def start(client, message):
     user = message.from_user
     await madflixbotz.add_user(client, message)                
     button = InlineKeyboardMarkup([[
-      InlineKeyboardButton('📢 Updates', url='https://t.me/Madflix_Bots'),
-      InlineKeyboardButton('💬 Support', url='https://t.me/MadflixBots_Support')
+      InlineKeyboardButton('📫 Updates', url='https://t.me/TheBotsLibrary'),
+      InlineKeyboardButton('☎️ Support', url='https://t.me/+hkLbiOernSsxYWQ1')
     ],[
-      InlineKeyboardButton('⚙️ Help', callback_data='help'),
-      InlineKeyboardButton('💙 About', callback_data='about')
+      InlineKeyboardButton('⁉️ Help', callback_data='help'),
+      InlineKeyboardButton('💻 About', callback_data='about')
     ],[
-        InlineKeyboardButton("🧑‍💻 Developer 🧑‍💻", url='https://t.me/CallAdminRobot')
+        InlineKeyboardButton("🧑‍💻 Developer 🧑‍💻", url='https://t.me/Otaku_Library_Support_Bot')
     ]])
     if Config.START_PIC:
         await message.reply_photo(Config.START_PIC, caption=Txt.START_TXT.format(user.mention), reply_markup=button)       
@@ -33,13 +33,13 @@ async def cb_handler(client, query: CallbackQuery):
             text=Txt.START_TXT.format(query.from_user.mention),
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup([[
-                InlineKeyboardButton('📢 Updates', url='https://t.me/Madflix_Bots'),
-                InlineKeyboardButton('💬 Support', url='https://t.me/MadflixBots_Support')
+                InlineKeyboardButton('📫 Updates', url='https://t.me/TheBotsLibrary'),
+                InlineKeyboardButton('☎️ Support', url='https://t.me/+hkLbiOernSsxYWQ1')
                 ],[
-                InlineKeyboardButton('⚙️ Help', callback_data='help'),
-                InlineKeyboardButton('💙 About', callback_data='about')
+                InlineKeyboardButton('⁉️ Help', callback_data='help'),
+                InlineKeyboardButton('💻 About', callback_data='about')
                 ],[
-                InlineKeyboardButton("🧑‍💻 Developer 🧑‍💻", url='https://t.me/CallAdminRobot')
+                InlineKeyboardButton("👷🏻‍♂️ Developer 👷🏻‍♂️", url='https://t.me/Otaku_Library_Support_Bot')
                 ]])
         )
     elif data == "caption":
@@ -56,10 +56,10 @@ async def cb_handler(client, query: CallbackQuery):
             text=Txt.HELP_TXT.format(client.mention),
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup([[
-                InlineKeyboardButton("⚙️ Setup AutoRename Format ⚙️", callback_data='file_names')
+                InlineKeyboardButton("🦧 Setup AutoRename Format 🦧", callback_data='file_names')
                 ],[
                 InlineKeyboardButton('🖼️ Thumbnail', callback_data='thumbnail'),
-                InlineKeyboardButton('✏️ Caption', callback_data='caption')
+                InlineKeyboardButton('✒️ Caption', callback_data='caption')
                 ],[
                 InlineKeyboardButton('🏠 Home', callback_data='home'),
                 InlineKeyboardButton('💰 Donate', callback_data='donate')
